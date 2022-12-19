05341273c0ef26df648237ffdce5814e68866083
Mona Mohammed <mona00mona1997@gmail.com>
2022-12-20 01:57:18 +0200

    new page

---
+++ js/toggle.js
@@ -0,0 +1,7 @@
+document.getElementById("login").addEventListener("click", function () {
+  document.getElementById("hide").style.display = "flex";
+});
+
+document.getElementById("hide").addEventListener("click", function () {
+  document.getElementById("hide").style.display = "none";
+});
